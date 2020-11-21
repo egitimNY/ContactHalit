2aa92727110e733392d1f29f0c4e342ded4db8fa
indirjavam <indirjava@gmail.com>
2020-11-21 12:40:26 -0500

    Initial commit

---
+++ app/src/main/java/com/halit/contacthalit/MainActivity.kt
@@ -0,0 +1,187 @@
+package com.halit.contacthalit
+
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_PHONE_CALL: Int= 1
+    // dbHelper
+    lateinit var dbHelper: MyDbHelper
+
+    // orderBy / sort queries
+    private val NEWEST_FIRST = "${Constants.C_ADDED_TIMESTAMP} DESC"
+    private val OLDEST_FIRST = "${Constants.C_ADDED_TIMESTAMP} ASC"
+    private val TITLE_ASC = "${Constants.C_NAME} ASC"
+    private val TITLE_DESC = "${Constants.C_NAME} DESC"
+
+    private var recentSortOrder = NEWEST_FIRST
+    private var number: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // init dbHelper
+        dbHelper = MyDbHelper(this)
+
+        loadRecords(NEWEST_FIRST) // by default load newest first
+
+        // click FloatingActionButton to start AddUpdateRecordActivity
+        addRecordBtn.setOnClickListener {
+            val intent = Intent(this, AddUpdateRecordActivity::class.java)
+            intent.putExtra("isEditMode", false) // want to add new record, set it false
+            startActivity(intent)
+        }
+    }
+
+    lateinit var  adapterRecord:AdapterRecord
+
+    private fun loadRecords(orderBy:String) {
+        recentSortOrder = orderBy
+        adapterRecord = AdapterRecord(this, dbHelper.getAllRecords(orderBy)) {
+            makeACall(it)
+        }
+
+        recordRv.adapter = adapterRecord
+    }
+
+    private fun searchRecords(query:String) {
+        adapterRecord = AdapterRecord(this, dbHelper.searchRecords(query)) {
+            makeACall(it)
+        }
+
+        recordRv.adapter = adapterRecord
+    }
+
+    private fun makeACall(number: String) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            this@MainActivity.number = number
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE),REQUEST_PHONE_CALL)
+        } else{
+            startCall(number)
+        }
+    }
+
+    private fun startCall(number: String) {
+        val intent =
+            Intent(Intent.ACTION_CALL, Uri.parse("tel:$number"))
+        startActivity(intent)
+
+    }
+
+    private fun sortDialog() {
+        // OPTIONS to display in dialog
+        val  options = arrayOf("Name Ascending", "Name Descending", "Newest", "Oldest")
+        // Dialog
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setItems(options){_, which ->
+            // Handle Items
+            if (which==0){
+                // Name Ascending
+                loadRecords(TITLE_ASC)
+            }else if(which==1){
+                // Name Descending
+                loadRecords(TITLE_DESC)
+            }else if (which==2){
+                // Newest
+                loadRecords(NEWEST_FIRST)
+            }else if (which==3){
+                // Oldest first
+                loadRecords(OLDEST_FIRST)
+            }
+        }
+            .show()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        loadRecords(recentSortOrder)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // inflate menu
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        // searchView
+        val item = menu.findItem(R.id.action_search)
+        val searchView = item.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // search as you type
+                if (newText != null){
+                    searchRecords(newText)
+                }
+
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // search when search button on keyboard is clicked
+                if (query != null){
+                    searchRecords(query)
+                }
+
+                return true
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle menu item clicks
+        val id= item.itemId
+        if (id==R.id.action_sort){
+            sortDialog()
+        }
+        else if (id==R.id.action_deleteAll){
+            // Delete all records
+            val alertDialog = AlertDialog.Builder(this).setTitle("Delete All").setMessage("Do you want to delete All")
+                .setPositiveButton("Ok",object:DialogInterface.OnClickListener{
+                    override fun onClick(dialog: DialogInterface?, which: Int) {
+                        dbHelper.deleteAllRecords()
+                        adapterRecord.notifyDataSetChanged()
+                    }
+                })
+                .setNegativeButton("Cancel", object:DialogInterface.OnClickListener{
+                    override fun onClick(dialog: DialogInterface?, which: Int) {
+                        dialog?.dismiss()
+                    }
+                })
+                .create()
+            alertDialog.show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_PHONE_CALL){
+            if(permissions[0]== Manifest.permission.CALL_PHONE){
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    startCall(number)
+                }
+            }
+        }
+    }
+
+}
+
---
+++ app/src/main/java/com/halit/contacthalit/RecordDetailActivity.kt
@@ -0,0 +1,145 @@
+package com.halit.contacthalit
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_record_detail.*
+import java.util.*
+
+class RecordDetailActivity : AppCompatActivity() {
+
+    private val REQUEST_PHONE_CALL: Int= 1
+
+    // actionBar
+    private var actionBar: ActionBar?=null
+
+    // dbHelper
+    private var dbHelper:MyDbHelper?=null
+
+    private  var recordId:String? = null
+
+    private var number = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_detail)
+
+        // setting up actionBar
+        actionBar = supportActionBar
+        actionBar!!.title = "Record Detail"
+        actionBar!!.setDisplayShowHomeEnabled(true)
+        actionBar!!.setDefaultDisplayHomeAsUpEnabled(true)
+
+        // init dbHelper
+        dbHelper = MyDbHelper(this)
+
+        // get record id from intent
+        val intent = intent
+        recordId = intent.getStringExtra("RECORD_ID")
+
+        showRecordDetail()
+
+    }
+
+    private fun showRecordDetail() {
+        // get record details
+
+        val selectQuery = "SELECT * FROM "+Constants.TABLE_NAME+ " WHERE "+ Constants.C_ID + " ="+recordId+""
+
+//        val db = dbHelper!!.writableDatabase
+        val db = dbHelper!!.writableDatabase
+        val cursor = db.rawQuery(selectQuery, null)
+
+        if (cursor.moveToFirst()){
+            do {
+
+                val id = ""+cursor.getInt(cursor.getColumnIndex(Constants.C_ID))
+                val name = ""+cursor.getString(cursor.getColumnIndex(Constants.C_NAME))
+                val image = ""+cursor.getString(cursor.getColumnIndex(Constants.C_IMAGE))
+                val bio = ""+cursor.getString(cursor.getColumnIndex(Constants.C_BIO))
+                val phone = ""+cursor.getString(cursor.getColumnIndex(Constants.C_PHONE))
+                val email = ""+cursor.getString(cursor.getColumnIndex(Constants.C_EMAIL))
+                val dob = ""+cursor.getString(cursor.getColumnIndex(Constants.C_DOB))
+                val addedTimeStamp = ""+cursor.getString(cursor.getColumnIndex(Constants.C_ADDED_TIMESTAMP))
+                val updatedTimeStamp = ""+cursor.getString(cursor.getColumnIndex(Constants.C_UPDATED_TIMESTAMP))
+
+                // convert timeStamp to dd//mm/yyyy e.g. 14/01/2020 08:22 AM
+                val calendar1 = Calendar.getInstance(Locale.getDefault())
+                calendar1.timeInMillis = addedTimeStamp.toLong()
+                val  timeAdded = android.text.format.DateFormat.format("dd/MM/yyyy hh:mm:aa", calendar1)
+
+                val calendar2 = Calendar.getInstance(Locale.getDefault())
+                calendar1.timeInMillis = updatedTimeStamp.toLong()
+                val  timeUpdated = android.text.format.DateFormat.format("dd/MM/yyyy hh:mm:aa", calendar2)
+
+
+                // set data
+                nameTv.text = name
+                bioTv.text = bio
+                phoneTv.text = phone
+                emailTv.text = email
+                dobTv.text = dob
+                addedDateTv.text = timeAdded
+                updatedDateTv.text = timeUpdated
+
+                main_item_contact.setOnClickListener {
+                    makeACall(phone)
+                }
+
+                // if user dosn't attach image then imageUri will be null, so set default image in that case
+                if (image == "null"){
+                    // no image in record, set default
+                    profileIv.setImageResource(R.drawable.ic_person_black)
+                }else{
+                    // have image in record
+                    profileIv.setImageURI(Uri.parse(image))
+                }
+
+            }while (cursor.moveToNext())
+        }
+        // close db connection
+        db.close()
+    }
+
+    private fun makeACall(number: String) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            this@RecordDetailActivity.number = number
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE),REQUEST_PHONE_CALL)
+        } else{
+            startCall(number)
+        }
+    }
+
+    private fun startCall(number: String) {
+        val intent =
+            Intent(Intent.ACTION_CALL, Uri.parse("tel:$number"))
+        startActivity(intent)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_PHONE_CALL){
+            if(permissions[0]== Manifest.permission.CALL_PHONE){
+                if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    startCall(number)
+                }
+            }
+        }
+    }
+
+}
