5ca7247c04441a318360b35c73c3afd86aa2691f
indirjavam <indirjava@gmail.com>
2020-12-18 23:46:35 -0500

    Login Register Forgot Password

---
+++ app/src/main/java/com/halit/contacthalit/loginregister/PasswordActivity.java
@@ -0,0 +1,63 @@
+package com.halit.contacthalit.loginregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.halit.contacthalit.R;
+
+public class PasswordActivity extends AppCompatActivity {
+
+    EditText username;
+    Button reset;
+    TextView signin;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+
+        username = findViewById(R.id.username_reset);
+        reset = findViewById(R.id.btnReset);
+        signin = findViewById(R.id.btnlogin);
+
+        DB = new DBHelper(this);
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user = username.getText().toString();
+
+                Boolean checkUser = DB.checkusername(user);
+                if (checkUser == true){
+
+                    Intent intent = new Intent(getApplicationContext(), ResetActivity.class);
+                    intent.putExtra("username", user);
+                    startActivity(intent);
+
+                }else {
+
+                    Toast.makeText(PasswordActivity.this, "User does not exists", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
